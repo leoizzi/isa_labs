a39cd8ff239fc141d82cb078fc5c5353473c5a63
ms20nicoleo <ms20nicoleo@gmail.com>
2020-11-10 11:41:13 +0100

    lab2 syn

--- lab2/syn_script.tcl
+++ lab2/syn_script.tcl
@@ -1,1 +1,1 @@
-read_file {./fpuvhdl/common/ ./fpuvhdl/adder/ ./fpuvhdl/multiplier/} -exclude {./fpuvhdl/multiplier/fpmul_single_cycle.vhdl} -autoread -format vhdl -top FPmul
+read_file {./fpuvhdl/common/ ./fpuvhdl/adder/ ./fpuvhdl/multiplier/} -exclude {./fpuvhdl/multiplier/fpmul_single_cycle.vhd} -autoread -format vhdl -top FPmul
