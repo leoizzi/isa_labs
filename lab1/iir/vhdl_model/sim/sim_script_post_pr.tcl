1df1e878dc862c8ef05bb66857fbb45e4a1758cb
NicoleDaiPra <nicoledaipra96@gmail.com>
2020-11-15 10:34:18 +0100

    riorganize folders

--- lab1/iir/vhdl_model/sim/sim_script_post_pr.tcl
+++ lab1/iir/vhdl_model/sim/sim_script_post_pr.tcl
@@ -2,16 +2,16 @@
 vcom -93 -work ./work ../tb/clk_gen.vhd
 vcom -93 -work ./work ../tb/data_maker_new.vhd
 vcom -93 -work ./work ../tb/data_sink.vhd
-vlog -work ./work ../netlist/iir.v
+vlog -work ./work ../innovus/iir.v
 vlog -work ./work ../tb/tb_fir.v
 
 # link the compiled library
 vsim -L /software/dk/nangate45/verilog/msim6.2g work.tb_iir
 
 # link the delay file
-vsim -L /software/dk/nangate45/verilog/msim6.2g -sdftyp /tb_iir/UUT=../netlist/iir.sdf work.tb_iir
+vsim -L /software/dk/nangate45/verilog/msim6.2g -sdftyp /tb_iir/UUT=../innovus/iir.sdf work.tb_iir
 
-vcd file ../vcd/iir_syn.vcd
+vcd file ../vcd/iir_design.vcd
 vcd add /tb_iir/UUT/*
 
 # simulate
