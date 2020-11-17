86ffc5c4c40d72e1a20dbc83518a9955f783a014
ms20nicoleo <ms20nicoleo@gmail.com>
2020-11-17 15:25:09 +0100

    part 1 almost finished

--- lab2/syn_script_pparch.tcl
+++ lab2/syn_script_pparch.tcl
@@ -1,6 +1,6 @@
 set power_preserve_rtl_hier_names true
 read_file {./fpuvhdl/common/ ./fpuvhdl/adder/ ./fpuvhdl/multiplier/} -exclude {./fpuvhdl/multiplier/fpmul_single_cycle.vhd} -autoread -format vhdl -top FPmul
-create_clock -name my_clk -period 1.6 clk
+create_clock -name my_clk -period 4.5 clk
 set_dont_touch_network my_clk
 set_clock_uncertainty 0.07 [get_clocks my_clk]
 set_input_delay 0.5 -max -clock my_clk [remove_from_collection [all_inputs] clk]
@@ -8,9 +8,10 @@ set_output_delay 0.5 -max -clock my_clk [all_outputs]
 set OLOAD [load_of NangateOpenCellLibrary/BUF_X4/A]
 set_load $OLOAD [all_outputs]
 ungroup -all -flatten
+set_implementation DW02_mult/pparch [find cell *mult*]
 compile
-report_resources > resources.txt
-report_timing > timing.txt
-report_area > area.txt
+report_resources > resources_pparch.txt
+report_timing > timing_pparch.txt
+report_area > area_pparch.txt
 quit
 
