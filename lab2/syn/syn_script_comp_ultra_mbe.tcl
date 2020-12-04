e76ddf04ae5429a1be8091a475379cf04caa4270
ms20nicoleo <ms20nicoleo@gmail.com>
2020-12-04 08:45:32 +0100

    synthesis completed

--- lab2/syn/syn_script_comp_ultra_mbe.tcl
+++ lab2/syn/syn_script_comp_ultra_mbe.tcl
@@ -1,6 +1,6 @@
 set power_preserve_rtl_hier_names true
 read_file {../fpuvhdl/common/ ../fpuvhdl/multiplier/} -exclude {../fpuvhdl/multiplier/fpmul_single_cycle.vhd ../fpuvhdl/multiplier/fpmul_stage2_struct.vhd  ../fpuvhdl/multiplier/fpmul_stage2_struct_mod.vhd ../fpuvhdl/multiplier/fpmul_stage2_struct_reg.vhd ../fpuvhdl/multiplier/dadda.vhd ../fpuvhdl/multiplier/mbe.vhd ../fpuvhdl/multiplier/fa.vhd ../fpuvhdl/multiplier/ha.vhd ../fpuvhdl/multiplier/mult.vhd} -autoread -format vhdl -top FPmul
-create_clock -name my_clk -period 0 clk
+create_clock -name my_clk -period 0.7 clk
 set_dont_touch_network my_clk
 set_clock_uncertainty 0.07 [get_clocks my_clk]
 set_input_delay 0.5 -max -clock my_clk [remove_from_collection [all_inputs] clk]
