ce896da54b40eac1a7a12f6a6900eec15f46f473
ms20nicoleo <ms20nicoleo@gmail.com>
2020-12-29 18:57:43 +0100

    synthesis done also for the riscv_abs

--- lab3/riscv_abs/syn/syn_script.tcl
+++ lab3/riscv_abs/syn/syn_script.tcl
@@ -1,6 +1,6 @@
 set power_preserve_rtl_hier_names true
 read_file {../src/} -exclude {../src/ram.vhd ../src/rom.vhd ../src/riscv_sim.vhd} -autoread -format vhdl -top core
-create_clock -name my_clk -period 1.8 clk
+create_clock -name my_clk -period 2.7 clk
 set_dont_touch_network my_clk
 set_clock_uncertainty 0.07 [get_clocks my_clk]
 set_input_delay 0.5 -max -clock my_clk [remove_from_collection [all_inputs] clk]
