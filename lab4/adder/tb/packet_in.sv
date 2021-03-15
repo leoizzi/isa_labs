58da65f18daeb197f790d8b7dcfa45f60238edfa
ms20nicoleo <ms20nicoleo@gmail.com>
2021-03-15 19:27:33 +0100

    adder tested

--- lab4/adder/tb/packet_in.sv
+++ lab4/adder/tb/packet_in.sv
@@ -2,6 +2,9 @@ class packet_in extends uvm_sequence_item;
     rand integer A;
     rand integer B;
 
+	constraint new_range {	A inside {[100:1000]};
+				B < 10*A; }
+
     `uvm_object_utils_begin(packet_in)
         `uvm_field_int(A, UVM_ALL_ON|UVM_HEX)
         `uvm_field_int(B, UVM_ALL_ON|UVM_HEX)
